32ea0d6637a34ebe4a03573b627a7e6f782f5c9b
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2020-11-12 15:57:41 +0100

    Bump version: 2.0.4 → 2.0.5

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/sjoerdk/idissend",
-    version="2.0.4",
+    version="version='2.0.5'",
     zip_safe=False,
 )

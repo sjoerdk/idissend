a91ae0790bb94a7150143c6615c1bd80a1af8b50
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2020-11-19 13:39:33 +0100

    Moves default instance method into pipeline module
    :w
    :wq
    :
    
    c

--- examples/pipeline.py
+++ examples/pipeline.py
@@ -5,117 +5,14 @@ be in a database for production
 """
 
 import logging
-from pathlib import Path
 
-from anonapi.client import AnonClientTool
-from anonapi.objects import RemoteAnonServer
-from anonapi.paths import UNCMapping, UNCMap, UNCPath
+from idissend.pipeline import get_pipeline_instance
 
-from idissend.core import Stream, Person, Stage
-from idissend.persistence import IDISSendRecords, get_db_sessionmaker
-from idissend.pipeline import DefaultPipeline
-from idissend.stages import CoolDown, PendingAnon, IDISConnection, Trash
-
-# parameters #
-BASE_PATH = Path("/tmp/idissend")  # all data for all stages goes here
-STAGES_BASE_PATH = BASE_PATH / "stages"
-
-DEFAULT_IDIS_PROFILE_NAME = "An_idis_profile"  # anonymize with this profile
-DEFAULT_PIMS_KEY = "123"  # Pass this to IDIS for generating pseudonyms
-
-IDIS_USERNAME = "SVC1234"  # use this to identify with IDIS web API
-IDIS_TOKEN = "a_token"
-
-IDIS_WEB_API_SERVER_URL = (
-    "https://umcradanonp11.umcn.nl/p01"  # Talk to IDIS through this
-)
-IDIS_WEB_API_SERVER_NAME = "p01"  # Name to use in log messages
-
-RECORDS_DB_URL = f"sqlite:///{STAGES_BASE_PATH / 'records_db.sqlite'}"
-
-OUTPUT_BASE_PATH = Path(r"\\server\path")  # let IDIS write all data here
-
-# init #
-STAGES_BASE_PATH.mkdir(parents=True, exist_ok=True)  # assert base dir exists
-
-# Indicate which local paths correspond to which UNC paths.
-# This makes it possible to expose local data to IDIS servers
-unc_mapping = UNCMapping([UNCMap(local=Path("/"), unc=UNCPath(r"\\server\share"))])
-
-# streams #
-# the different routes data can take through the pipeline. Data will always stay
-# inside the same stream
-streams = [
-    Stream(
-        name="stream1",
-        output_folder=OUTPUT_BASE_PATH / "stream1",
-        idis_profile_name=DEFAULT_IDIS_PROFILE_NAME,
-        pims_key=DEFAULT_PIMS_KEY,
-        contact=Person(name="Sjoerd", email="mock_email"),
-    ),
-    Stream(
-        name="stream2",
-        output_folder=OUTPUT_BASE_PATH / "stream2",
-        idis_profile_name=DEFAULT_IDIS_PROFILE_NAME,
-        pims_key=DEFAULT_PIMS_KEY,
-        contact=Person(name="Sjoerd2", email="mock_email"),
-    ),
-]
-
-# stages #
-# data in one stream goes through one or more of these stages
-incoming = CoolDown(
-    name="incoming", path=STAGES_BASE_PATH / "incoming", streams=streams, cool_down=0
-)
-
-cooled_down = Stage(
-    name="cooled_down", path=STAGES_BASE_PATH / "cooled_down", streams=streams
-)
-
-connection = IDISConnection(
-    client_tool=AnonClientTool(username=IDIS_USERNAME, token=IDIS_TOKEN),
-    servers=[
-        RemoteAnonServer(name=IDIS_WEB_API_SERVER_NAME, url=IDIS_WEB_API_SERVER_URL)
-    ],
-)
-
-records = IDISSendRecords(session_maker=get_db_sessionmaker(RECORDS_DB_URL))
-
-pending = PendingAnon(
-    name="pending",
-    path=STAGES_BASE_PATH / "pending",
-    streams=streams,
-    idis_connection=connection,
-    records=records,
-    unc_mapping=unc_mapping,
-)
-
-errored = Stage(name="errored", path=STAGES_BASE_PATH / "errored", streams=streams)
-
-finished = CoolDown(
-    name="finished",
-    path=STAGES_BASE_PATH / "finished",
-    streams=streams,
-    cool_down=2 * 60 * 24,
-)  # 2 days
-
-trash = Trash(name="trash", path=STAGES_BASE_PATH / "trash", streams=streams)
-
-pipeline = DefaultPipeline(
-    incoming=incoming,
-    cooled_down=cooled_down,
-    pending=pending,
-    finished=finished,
-    trash=trash,
-    errored=errored,
-)
-
-
-logging.basicConfig(format="%(asctime)s %(study_id)-40s %(levelname)-8s %(message)s")
+# Make sure you see some logging output
+logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-logger.info("Running once")
-
-pipeline.incoming.assert_all_paths()
+pipeline = get_pipeline_instance(base_path="/tmp/idissend")
+pipeline.assert_all_paths()
 pipeline.run_once()

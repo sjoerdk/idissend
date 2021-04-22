12e8c3b67301a48e055314127a771a0d064586cc
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2021-04-22 12:29:16 +0200

    Updates mysql imports

--- setup.py
+++ setup.py
@@ -24,13 +24,12 @@ test_requirements = [
 setup(
     author="Sjoerd Kerkstra",
     author_email="w.s.kerkstra@gmail.com",
-    python_requires="~=3.6",
+    python_requires="~=3.7",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],

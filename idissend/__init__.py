63c995ea4bbf3620f0d274f15694f4049478a3cb
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2021-04-22 12:29:21 +0200

    Bump version: 2.2.2 → 2.2.3

--- idissend/__init__.py
+++ idissend/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Sjoerd Kerkstra"""
 __email__ = "w.s.kerkstra@gmail.com"
-__version__ = "2.2.2"
+__version__ = "2.2.3"
--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/sjoerdk/idissend",
-    version="2.2.2",
+    version="2.2.3",
     zip_safe=False,
 )
